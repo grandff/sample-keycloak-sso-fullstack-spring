b3d757bedfd62aa681b989972da07624b0f6b25d
grandff <dsaqaz421@naver.com>
2024-01-29 13:09:14 +0900

    Feat: logout template add

--- sample-client/src/main/java/com/kjm/client/sampleclient/auth/controller/MemberPageController.java
+++ sample-client/src/main/java/com/kjm/client/sampleclient/auth/controller/MemberPageController.java
@@ -1,9 +1,12 @@
 package com.kjm.client.sampleclient.auth.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -40,6 +43,30 @@ public class MemberPageController {
     private WebClient webClient;
 
     private static final Logger log = LoggerFactory.getLogger(MemberPageController.class);
+
+    // 로그인 화면으로 이동
+    @GetMapping("/login")
+    public String goToLogin(HttpServletRequest request, HttpServletResponse response, Model model)
+            throws UnsupportedEncodingException {
+        String code = request.getParameter("code");
+        String query = "code=" + URLEncoder.encode(code, "UTF-8");
+        query += "&client_id=" + "ssoClient-1";
+        query += "&client_secret=" + "KkGxR78OdKBdWgAmeecgfI7UaUgZ6buX";
+        query += "&redirect_uri=" + "http://localhost:8082/ui-one"; // 너님들이 설정해야할 redirect_uri
+        query += "&grant_type=authorization_code";
+
+        webClient.post()
+                .uri("http://localhost:8083/auth/realms/dev/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(query)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+                })
+                .block();
+
+        return "redirect:/";
+    }
 
     // 회원가입 화면으로 이동
     @GetMapping("/register")
@@ -96,7 +123,8 @@ public class MemberPageController {
 
     // 회원가입
     @PostMapping("/register")
-    public String registerMember(@Valid MemberRegsiterRequestDto memberRegsiterRequestDto, BindingResult result, HttpSession session, Model model, RedirectAttributes redirectAttributes) {
+    public String registerMember(@Valid MemberRegsiterRequestDto memberRegsiterRequestDto, BindingResult result,
+            HttpSession session, Model model, RedirectAttributes redirectAttributes) {
         // 회원가입 유효성 error시 회원가입화면으로 return
         if (result.hasErrors()) {
             return "pages/member/register";
@@ -107,7 +135,7 @@ public class MemberPageController {
 
         // WebClient를 사용하여 POST 요청 보내기
         String url = fooApiUrl + "/member/v1.0";
-        
+
         Map<String, Object> resultMap = webClient.post()
                 .uri(url)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -129,7 +157,6 @@ public class MemberPageController {
         return "redirect:/";
     }
 
-
     // 로그아웃
     @GetMapping("/logout")
     public String logout(HttpServletRequest request) throws ServletException {
@@ -137,7 +164,7 @@ public class MemberPageController {
         return "redirect:http://localhost:8083/auth/realms/dev/protocol/openid-connect/logout?redirect_uri=http://localhost:8082/ui-one";
     }
 
-    ///프로필 화면 이동
+    /// 프로필 화면 이동
     @GetMapping("/profile/view")
     public String viewUserProfile(Model model) {
         Map<String, Object> memberInfo = this.webClient.get()
@@ -145,12 +172,12 @@ public class MemberPageController {
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
                 })
-        .block();
+                .block();
         model.addAttribute("memberInfo", memberInfo);
         return "pages/member/profile/view";
     }
 
-    ///프로필 화면 이동
+    /// 프로필 화면 이동
     @GetMapping("/profile/update")
     public String updateUserProfile(Model model) {
         Map<String, Object> memberInfo = this.webClient.get()
@@ -158,9 +185,9 @@ public class MemberPageController {
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
                 })
-        .block();
-        
-        //정보매핑하기 
+                .block();
+
+        // 정보매핑하기
         Map<String, String> data = (Map<String, String>) memberInfo.get("data");
         MemberRegsiterRequestDto profileUpdateRequestDto = new MemberRegsiterRequestDto();
         profileUpdateRequestDto.setUsername(data.get("username"));
@@ -169,29 +196,29 @@ public class MemberPageController {
         profileUpdateRequestDto.setCity(data.get("city"));
         profileUpdateRequestDto.setAddr(data.get("addr"));
         profileUpdateRequestDto.setBio(data.get("bio"));
-        
+
         model.addAttribute("profileUpdateRequestDto", profileUpdateRequestDto);
-       
+
         return "pages/member/profile/update";
     }
 
-    ///프로필 저장
+    /// 프로필 저장
     @PostMapping("/profile/save")
-    public String saveUserProfile(@Valid MemberRegsiterRequestDto profileUpdateRequestDto, BindingResult result,  HttpSession session, Model model, RedirectAttributes redirectAttributes) {
+    public String saveUserProfile(@Valid MemberRegsiterRequestDto profileUpdateRequestDto, BindingResult result,
+            HttpSession session, Model model, RedirectAttributes redirectAttributes) {
         // WebClient를 사용하여 POST 요청 보내기
-        String url = fooApiUrl + "/member/profile/v1.0";      
+        String url = fooApiUrl + "/member/profile/v1.0";
         Map<String, Object> resultMap = webClient.post()
-            .uri(url)
-            .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(profileUpdateRequestDto)
-            .retrieve()
-            .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
-            })
-        .block();
-        System.out.println("resultMap : "+ resultMap.toString());
+                .uri(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(profileUpdateRequestDto)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+                })
+                .block();
+        System.out.println("resultMap : " + resultMap.toString());
         return "redirect:/member/profile/view";
 
     }
-
 
 }
